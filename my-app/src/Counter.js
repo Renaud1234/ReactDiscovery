12d896ed1cbaeb167603d8240de70cc35e9a4706
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-07-01 16:15:02 +0100

    Counter using Hooks

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Counter extends React.Component{
-    state={
-      counter:0
+function Counter() {
+    const [counter, setCounter] = useState(0);
+  
+    function increment() {
+      setCounter(counter+1);
     }
-    increment = () => {
-        this.setState({counter: this.state.counter+1});
-    }
-    render(){
-      return <div>
-        <p>{this.state.counter}</p>
-        <button onClick={this.increment}>Increment</button>
-      </div>
-    }
-  }
+  
+    return <div>
+    <p>{counter}</p>
+    <button onClick={increment}>
+      Increment
+    </button>
+    </div>;
+  } 
 
-  export default Counter;
+export default Counter;
